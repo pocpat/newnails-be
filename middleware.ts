c1c6a2fb0ac3d6f3b49c5a98ff95641bdb1aa366
Elena Kroupkin <pocoat@gmail.com>
2025-07-15 22:05:56 +1200

    updated the middleware.ts file to correctly use createRouteMatcher to make   /api/save-design a public route.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  publicRoutes: ['/api/save-design'],
+const isPublicRoute = createRouteMatcher([
+  '/api/save-design',
+]);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
 });
 
 export const config = {
